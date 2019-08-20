9fddf6a147f8329a8a1054cac1b812e2730c7dad
Trisha Atluri <trishaatluri@trishas-mbp.private.wireless.med.harvard.edu>
2019-08-20 14:27:43 -0400

    Updates

---
+++ precache-manifest.0e621566f31b56b7d9dc474e5c2acb1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39dfd60eb85f90e728666e3110c5fb7",
+    "url": "/dnawebmodule/index.html"
+  },
+  {
+    "revision": "75cccdd3ab175f28a11b",
+    "url": "/dnawebmodule/static/css/main.41ceb250.chunk.css"
+  },
+  {
+    "revision": "9cc6099bd0c37a07ba51",
+    "url": "/dnawebmodule/static/js/2.f2ba50c5.chunk.js"
+  },
+  {
+    "revision": "75cccdd3ab175f28a11b",
+    "url": "/dnawebmodule/static/js/main.280ca540.chunk.js"
+  },
+  {
+    "revision": "926cfb975ae2501e81c8",
+    "url": "/dnawebmodule/static/js/runtime~main.a40b5e1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dnawebmodule/static/media/logo.5d5d9eef.svg"
+  }
+]);
